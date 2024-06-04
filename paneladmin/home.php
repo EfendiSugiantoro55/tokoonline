d32f33a571a4efeeb1b789f1b1b289016e3bfb07
Efendi-Sugiantoro-16 <efendisugiantoro55@gmail.com>
2024-06-04 11:29:15 +0700

    Upload File Diperbaharui

---
+++ paneladmin/home.php
@@ -0,0 +1,7 @@
+<h3><i class="fa fa-angle-right"></i> Selamat Datang <?= $_SESSION['nama_lengkap']; ?></h3>
+<div class="row mt">
+  <div class="col-lg-12">
+    <p><pre>Aplikasi Website Sederhana Toko Online EFD | Repost by <a href='https://efendi.com/' title='efendi.com' target='_blank'>efendi.com</a>
+    </pre>.</p>
+  </div>
+</div>
